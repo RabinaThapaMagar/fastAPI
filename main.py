26897b0ca06c97cbb6d324392596f341a7aecea5
Rabina Thapa Magar <rabinathapa38@gmail.com>
2025-07-16 15:57:40 +0545

    FastAPI(day2)

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from fastapi import  FastAPI
+from pydantic import BaseModel  ##model lai data pydantic ko through bata dinxan data model lai
+import joblib
+import numpy as np
+
 
 app = FastAPI() 
+
+#loading the model
+model = joblib.load('saved/DecisionTreePrediction.pkl')
+
+#datako type milauna parxa using pydantic
+
+#Glucose
+#BloodPressure
+#DFP
+#AGE
+
+#datako lagi class create garne
+class Patient(BaseModel) :
+    Glucose: float
+    BloodPressure: int
+    DiabetesPedigreeFunction : float
+    Age : int
+
 
 @app.get('/')## route banako mathiko app object use garera
 def show(): ## show vaney function
@@ -11,3 +33,15 @@ def show(): ## show vaney function
 @app.get('/about')
 def about():
     return{'Display':'Broad AI is a education Sector'}
+
+@app.post('/predict')
+async def predict(data:Patient): #async asynchronouse banako function ko name ko agadi
+    input_data = np.array([[
+        data.Glucose, data.BloodPressure, data.DiabetesPedigreeFunction, data.Age
+
+   ]]) 
+    prediction = await model.predict(input_data)[0] #await ie free time payo bhaney arko lai ready banaune for prediction
+    return {"prediction": int(prediction)}
+
+
+
